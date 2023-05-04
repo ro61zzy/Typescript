d6e21e605bd0024b336d1a33ba08eebad8135be6
Rozzy <rosekaremeri@gmail.com>
2023-05-04 13:52:30 +0300

    enums

---
+++ Basics/dist/index3.js
@@ -0,0 +1,13 @@
+"use strict";
+const small = 1;
+const medium = 2;
+const large = 3;
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+console.log(mySize);
+//# sourceMappingURL=index3.js.map
--- Basics/src/index3.ts
+++ Basics/src/index3.ts
@@ -1,2 +1,11 @@
 //enums, this is a new one - it represents a list of related constants
 //instead of 
+const small = 1
+const medium = 2
+const large = 3
+
+//you can use enum
+enum Size {Small = 1, Medium, Large}
+//by default the first value of the list will be 0 but if you don't want this , you can explicitly set the values Small = 1, if not a number, then you have to explicitly set a value for each of them as ts will not infer Small = 's', Medium = 'm' and so on. Now we can declare a variable
+let mySize: Size = Size.Medium
+console.log(mySize) // this will give answer 2
