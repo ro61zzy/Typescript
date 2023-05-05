60ad8abded7466428726e5543ec2dcdddc142697
Rozzy <rosekaremeri@gmail.com>
2023-05-05 13:43:43 +0300

    working with objects

--- Basics/src/index5.ts
+++ Basics/src/index5.ts
@@ -3,8 +3,13 @@
 let employee: {
   readonly id: number;
   name: string;
+  retire: (date: Date) => void
 } = {
   id: 2,
   name: "rossie",
+  retire: (date: Date) => {
+    console.log(date)
+  }
 };
 //to make a property contant, that is not to be changed later, we use readonly property while setting it like in id, now, you cannot change the value of id
+// so now, how can we define a function here
